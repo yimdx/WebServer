f1419414599aba2e6cf5a7c3b31b72cb6806a122
xuefengli <li.xuefen@northeastern.com>
2025-05-13 17:16:24 -0700

    use RAII rewrite mutex lock

--- src/threadpool/thread_pool.cpp
+++ src/threadpool/thread_pool.cpp
@@ -8,12 +8,14 @@ ThreadPool::ThreadPool(size_t max_thread_num)
     for(int i = 0; i < max_thread_num; i++){
         _thread_pool.emplace_back([this](){
             while(true){
-                std::unique_lock<std::mutex> lk(_task_mutex);
-                _task_cond.wait(lk, [this](){return _stop && _task_queue.empty();});
-                if(_stop) return;
-                auto task = std::move(_task_queue.front());
-                _task_queue.pop();
-                lk.unlock();
+                std::function<void()> task;
+                {
+                    std::unique_lock<std::mutex> lk(_task_mutex);
+                    _task_cond.wait(lk, [this](){return _stop && _task_queue.empty();});
+                    if(_stop) return;
+                    task = std::move(_task_queue.front());
+                    _task_queue.pop();
+                }
                 task();
             }
         });
@@ -21,9 +23,10 @@ ThreadPool::ThreadPool(size_t max_thread_num)
 }
 
 void ThreadPool::stop(){
-    std::unique_lock<std::mutex> lk(_task_mutex);
-    _stop = true;
-    lk.unlock();
+    {
+        std::unique_lock<std::mutex> lk(_task_mutex);
+        _stop = true;
+    }
     _task_cond.notify_all();
     for(int i = 0; i < _thread_pool.size(); i++){
         _thread_pool[i].join();
